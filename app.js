ffca68888875cd07778bcf7d7f8c2356b655a047
alfalfaw <limuyle@163.com>
2020-05-28 18:46:18 +0800

    修改cdn,提高速度

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const fileUpload = require("express-fileupload");
 const exphbs = require("express-handlebars");
 const http = require("http");
 const socketio = require("socket.io");
-
-const PORT = process.env.PORT || 5000;
+const compression = require("compression");
+const PORT = process.env.PORT || 5010;
 
 const app = express();
 // 如果使用websocket，必须使用http的createServer()创建http server
@@ -16,7 +16,8 @@ const io = socketio(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+// 页面gzip压缩
+app.use(compression());
 // 文件上传
 app.use(fileUpload());
 
@@ -24,7 +25,10 @@ app.use(fileUpload());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Static folder
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(
+  "/public",
+  express.static(path.join(__dirname, "public"), { maxAge: 31557600 })
+);
 
 // 连接mongodb
 require("./database/mongodb")();
